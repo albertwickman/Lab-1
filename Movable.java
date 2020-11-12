3165f4cd638886ddb740ccb60ec564865f12b9c3
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2020-11-12 11:02:39 +0100

    Javadocs in Movable

--- Movable.java
+++ Movable.java
@@ -1,5 +1,19 @@
+/**
+ * The interface Movable.
+ */
 public interface Movable {
+    /**
+     * Move.
+     */
     void move();
+
+    /**
+     * Turn left.
+     */
     void turnLeft();
+
+    /**
+     * Turn right.
+     */
     void turnRight();
 }
