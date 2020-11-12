5129572f48fb02ae45be7ef2ef1a41d72a3e26b7
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2020-11-12 12:31:19 +0100

    Test for Saab95

--- test/TestSaab95.java
+++ test/TestSaab95.java
@@ -18,6 +18,31 @@ public class TestSaab95 {
     public void gasNoTurbo() {
         Saab95 saab = new Saab95(0,0);
         saab.gas(20);
-        assertEquals();
+        assertEquals(0, Double.compare(saab.getCurrentSpeed(), 25));
     }
+
+    @Test
+    public void gasTurbo() {
+        Saab95 saab = new Saab95(0,0);
+        saab.setTurboOn();
+        saab.gas(20);
+        assertEquals(0, Double.compare(saab.getCurrentSpeed(), 32.5));
+    }
+
+    @Test
+    public void turbo() {
+        Saab95 saab = new Saab95(0,0);
+        saab.setTurboOn();
+        saab.setTurboOff();
+        assertFalse(saab.isTurboOn());
+    }
+
+    @Test
+    public void brakeTest() {
+        Saab95 saab = new Saab95(0,0);
+        saab.gas(20);
+        saab.brake(10);
+        assertEquals(0, Double.compare(saab.getCurrentSpeed(), 12.5));
+    }
+
 }
