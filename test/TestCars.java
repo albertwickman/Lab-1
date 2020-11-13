22f7c8c8741a1a3620592048133afcb025cdba45
Pongag <vallemannen@live.se>
2020-11-13 13:49:51 +0100

    Added method testTurnLeft in TestCars

--- test/TestCars.java
+++ test/TestCars.java
@@ -1,9 +1,21 @@
 import org.junit.Test;
 import src.Cars;
+import src.Volvo240;
+
+import java.awt.*;
 
 import static org.junit.Assert.*;
 
 public class TestCars {
 
+    Volvo240 car = new Volvo240(50, 50);
 
+    @Test
+    public void testTurnLeft() {
+        car.startEngine();
+        car.gas(0.5);
+        car.turnLeft();
+        //assertEquals(0, Double.compare());
+
+    }
 }
