d8ee11cc60ecf4f93d67d40d9007ba500a8dda93
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2020-11-19 18:27:04 +0100

    Tests for Transport

---
+++ test/TestTransport.java
@@ -0,0 +1,29 @@
+import org.junit.*;
+import src.Cars;
+import src.Saab95;
+import src.Scania;
+import src.Transport;
+
+import static org.junit.Assert.*;
+import java.awt.*;
+import java.util.Arrays;
+
+public class TestTransport {
+    @Test
+    public void isTransport(){
+        Scania s = new Scania(2, 400, 0, Color.red, "BigAssTruck", 0,0);
+        s.connectTransport(6);
+        assertTrue();
+    }
+
+    @Test
+    public void oneCar() {
+        Scania s = new Scania(2, 400, 0, Color.red, "BigAssTruck", 0,0);
+        s.connectTransport(6);
+        Saab95 saab = new Saab95(0,0);
+        s.loadCar(saab);
+        Cars[] c = s.getLoadedCars();
+        System.out.println(Arrays.toString(c));
+        assertEquals(saab, c[0]);
+    }
+}
