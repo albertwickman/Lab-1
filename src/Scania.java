40c946ecbbdcc95b1ec62d93702fb2ddbe5d7563
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2020-11-19 16:28:17 +0100

    Updated Scania

--- src/Scania.java
+++ src/Scania.java
@@ -15,14 +15,22 @@ public class Scania extends Vehicle {
      * @param color        the color
      * @param modelName    the model name
      */
-    public Scania(int nrDoors, double enginePower, double currentSpeed, Color color, String modelName, int xCor, int yCor) {
+    public Scania(int nrDoors, double enginePower, double currentSpeed, Color color, String modelName, int xCor, int yCor, Trailer trailer) {
         super(nrDoors, enginePower, currentSpeed, color, modelName, xCor, yCor);
+        this.trailer = trailer;
     }
 
+    /**
+     * Change the trailer to a transport.
+     * @param maxCars Maximum capacity of the trailer
+     */
     public void convertToTransport(int maxCars) {
         trailer = new Transport(this, maxCars);
     }
 
+    /**
+     * Change the trailer to a platform.
+     */
     public void convertToPlatform() {
         trailer = new Platform(this);
     }
