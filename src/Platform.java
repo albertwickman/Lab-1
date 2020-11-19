881463c69df5bd48d4cc0fdc20e7b04632d6af9f
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2020-11-19 14:26:01 +0100

    Updated Platform and created Transport

--- src/Platform.java
+++ src/Platform.java
@@ -1,21 +1,26 @@
 package src;
 
-public class Platform extends Trailer{
+public class Platform {
     private int angle;
     private final Scania truck;
+    private final int MIN_ANGLE = 0;
+    private final int MAX_ANGLE = 0;
 
+    /**
+     * Creates an instance of platform.
+     * @param truck
+     */
     public Platform (Scania truck) {
         this.angle = 0;
         this.truck = truck;
 
     }
 
-    public void raisePlatform(int angle) {
-        if (0 <= angle && angle <= 70)
+    public void setAngle(int angle) {
+        if (MIN_ANGLE <= angle && angle <= MAX_ANGLE) {
             this.angle = angle;
-    }
-
-    public void lowerPlatform(int angle) {
-        if (0)
+            if (angle != MIN_ANGLE)
+                truck.setRaised(true);
+        }
     }
 }
