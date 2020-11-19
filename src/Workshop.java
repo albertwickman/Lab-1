8409d524956fa972ad3014254da2acf00ff48436
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2020-11-19 17:22:33 +0100

    Update Workshop

---
+++ src/Workshop.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Workshop<C extends Cars> {
+    public List<C> car;
+
+    public Workshop(List<C> car) {
+        this.car = car;
+    }
+
+    public void load(Car)
+
+}
