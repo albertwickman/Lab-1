1229c589e0a87fa879d26e573ad21f10f5697598
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2020-11-20 10:46:06 +0100

    Docs for Cars

--- src/Cars.java
+++ src/Cars.java
@@ -2,7 +2,17 @@ package src;
 
 import java.awt.*;
 
-public class Cars extends Vehicle {
+public abstract class Cars extends Vehicle {
+    /**
+     * Abstract class representing a car extending Vehicle
+     * @param nrDoors Number of doors
+     * @param enginePower Horsepower
+     * @param currentSpeed Current speed in km/h
+     * @param color Color of the car
+     * @param modelName Model name
+     * @param xCor x-coordinate
+     * @param yCor y-coordinate
+     */
     public Cars(int nrDoors, double enginePower, double currentSpeed, Color color,
                 String modelName, int xCor, int yCor){
         super(nrDoors, enginePower, currentSpeed, color,
