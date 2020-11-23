257eac87dde1cd04c6790b4f8e599d76b1826ac2
Pongag <vallemannen@live.se>
2020-11-23 17:00:17 +0100

    Documented Scania class.

--- src/Scania.java
+++ src/Scania.java
@@ -2,25 +2,28 @@ package src;
 
 import java.awt.*;
 
+/**
+ * Class for a Scania truck. Extends Truck and implements a ramp.
+ */
+
 public class Scania extends Truck implements Ramp{
+    /**
+     * Ramp has a min angle and a max angle.
+     *  angle is current angle of ramp.
+     */
     private final int MIN_ANGLE = 0;
     private final int MAX_ANGLE = 70;
     private int angle;
-    /**
-     * Instantiates a new Car.
-     *
-     * @param nrDoors      the nr doors
-     * @param enginePower  the engine power
-     * @param currentSpeed the current speed
-     * @param color        the color
-     * @param modelName    the model name
-     */
+
     public Scania(int nrDoors, double enginePower, double currentSpeed, Color color, String modelName, int xCor, int yCor) {
         super(nrDoors, enginePower, currentSpeed, color, modelName, xCor, yCor);
         setMovementAllowed(true);
         this.angle = MIN_ANGLE;
     }
 
+    /**
+     * Moves truck if movement is allowed, i.e. the ramp is up.
+     */
     @Override
     public void move() {
         if (isMovementAllowed()) {
@@ -28,6 +31,10 @@ public class Scania extends Truck implements Ramp{
         }
     }
 
+    /**
+     * Sets angle of ramp.
+     * @param angle to set ramp at angle
+     */
     @Override
     public void setAngle(int angle) {
         if (getCurrentSpeed() == 0 && MIN_ANGLE <= angle && angle <= MAX_ANGLE) {
