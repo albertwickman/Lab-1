7b7a3dd813134dee738662360e3c7656d8d07665
Pongag <vallemannen@live.se>
2020-12-09 16:53:22 +0100

    Added setters and getters in Model

--- src/Model.java
+++ src/Model.java
@@ -1,13 +1,31 @@
 package src;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Set;
 
 public class Model {
 
-    Set<ModelObserver> modelObservers;
-    ArrayList<Vehicle> vehicles;
-    ArrayList<Ramp> ramps;
-    ArrayList<Turbo> turbos;
+    private Set<ModelObserver> modelObservers;
+    private ArrayList<Vehicle> vehicles;
+    private ArrayList<Ramp> ramps;
+    private ArrayList<Turbo> turbos;
+
+    public Set<ModelObserver> getModelObservers() { return this.modelObservers;}
+
+    public void addModelObserver(ModelObserver modelObserver) {
+        this.modelObservers.add(modelObserver);
+    }
+
+    public ArrayList<Vehicle> getVehicles() { return vehicles; }
+
+    public void addVehicles(Vehicle vehicle) { this.vehicles.add(vehicle); }
+
+    public ArrayList<Ramp> getRamps() { return ramps; }
+
+    public void addRamp(Ramp ramp) { this.ramps.add(ramp); }
+
+    public ArrayList<Turbo> getTurbos() { return turbos; }
+
+    public void addTurbo(Turbo turbo) { this.turbos.add(turbo); }
+
 }
