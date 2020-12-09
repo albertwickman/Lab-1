b0118813cc9a1d05e8e7495dfa1a9331ec280d79
Pongag <vallemannen@live.se>
2020-12-09 16:54:26 +0100

    formatting

--- src/Model.java
+++ src/Model.java
@@ -10,22 +10,19 @@ public class Model {
     private ArrayList<Ramp> ramps;
     private ArrayList<Turbo> turbos;
 
-    public Set<ModelObserver> getModelObservers() { return this.modelObservers;}
 
+    public Set<ModelObserver> getModelObservers() { return this.modelObservers;}
     public void addModelObserver(ModelObserver modelObserver) {
         this.modelObservers.add(modelObserver);
     }
 
     public ArrayList<Vehicle> getVehicles() { return vehicles; }
-
     public void addVehicles(Vehicle vehicle) { this.vehicles.add(vehicle); }
 
     public ArrayList<Ramp> getRamps() { return ramps; }
-
     public void addRamp(Ramp ramp) { this.ramps.add(ramp); }
 
     public ArrayList<Turbo> getTurbos() { return turbos; }
-
     public void addTurbo(Turbo turbo) { this.turbos.add(turbo); }
 
 }
